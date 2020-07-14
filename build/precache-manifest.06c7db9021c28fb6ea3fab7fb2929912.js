138f6ef1d3b2999ecd54ec91d810168848472f02
“Matias <matias@capicua.com.uy>
2020-07-14 20:32:56 -0300

    build all staff

---
+++ build/precache-manifest.06c7db9021c28fb6ea3fab7fb2929912.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047d419e8a480abba47999c15e6419a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e4f0d8ac9fa5e284883",
+    "url": "/static/css/2.1d984fbf.chunk.css"
+  },
+  {
+    "revision": "1472210418202255fa1f",
+    "url": "/static/css/main.f567d74a.chunk.css"
+  },
+  {
+    "revision": "5e4f0d8ac9fa5e284883",
+    "url": "/static/js/2.520dc9d4.chunk.js"
+  },
+  {
+    "revision": "7dcc9140afd9732098f02889cf011cf4",
+    "url": "/static/js/2.520dc9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1472210418202255fa1f",
+    "url": "/static/js/main.e7a8f100.chunk.js"
+  },
+  {
+    "revision": "a3a64586e9dc8c4700ac",
+    "url": "/static/js/runtime-main.f7decd78.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "c9b3e104479e2fab5588080e81dfb689",
+    "url": "/static/media/ag-logo.c9b3e104.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/static/media/icons.efe053d4.ttf"
+  }
+]);
